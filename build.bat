e3d4838b4e1c9ee6c46e07a32b99a65ea2c7d799
JezzX2 <JezzX2@0c45cd24-f56c-ec91-222f-6e3ca35936fa>
2010-10-07 08:07:36 +0000

    Fixed : [SLik] build.bat
    git-svn-id: http://jezzxbmc.googlecode.com/svn/trunk/skin.SLik@114 0c45cd24-f56c-ec91-222f-6e3ca35936fa

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 ECHO ----------------------------------------
 echo Creating Build Folder
 rmdir BUILD /S /Q
-md BUILD\skin.pm3.hd\media\
+md BUILD\skin.SLik\media\
 
 Echo .svn>exclude.txt
 Echo Thumbs.db>>exclude.txt
@@ -12,21 +12,21 @@ Echo exclude.txt>>exclude.txt
 
 ECHO ----------------------------------------
 ECHO Creating XBT File...
-START /B /WAIT ..\..\TexturePacker\TexturePacker -input media -output BUILD\skin.pm3.hd\media\Textures.xbt
+START /B /WAIT ..\..\TexturePacker\TexturePacker -input media -output BUILD\skin.SLik\media\Textures.xbt
 
 ECHO ----------------------------------------
 ECHO XBT Texture Files Created...
 ECHO Building Skin Directory...
-xcopy "720p" "BUILD\skin.pm3.hd\720p" /E /Q /I /Y /EXCLUDE:exclude.txt
-xcopy "fonts" "BUILD\skin.pm3.hd\fonts" /E /Q /I /Y /EXCLUDE:exclude.txt
-xcopy "backgrounds" "BUILD\skin.pm3.hd\backgrounds" /E /Q /I /Y /EXCLUDE:exclude.txt
-xcopy "sounds\*.*" "BUILD\skin.pm3.hd\sounds\" /Q /I /Y /EXCLUDE:exclude.txt
-xcopy "colors\*.*" "BUILD\skin.pm3.hd\colors\" /Q /I /Y /EXCLUDE:exclude.txt
-xcopy "language" "BUILD\skin.pm3.hd\language" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "720p" "BUILD\skin.SLik\720p" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "fonts" "BUILD\skin.SLik\fonts" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "backgrounds" "BUILD\skin.SLik\backgrounds" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "sounds\*.*" "BUILD\skin.SLik\sounds\" /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "colors\*.*" "BUILD\skin.SLik\colors\" /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "language" "BUILD\skin.SLik\language" /E /Q /I /Y /EXCLUDE:exclude.txt
 
 del exclude.txt
 
-copy *.xml "BUILD\skin.pm3.hd\"
-copy *.txt "BUILD\skin.pm3.hd\"
-copy icon.png "BUILD\skin.pm3.hd\"
+copy *.xml "BUILD\skin.SLik\"
+copy *.txt "BUILD\skin.SLik\"
+copy icon.png "BUILD\skin.SLik\"
 pause
